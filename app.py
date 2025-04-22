cc6b341a5c4a01b0c1a2804c1c5fada1969b082d
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-04-22 19:23:47 +0530

    feat: task adding option

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,10 +15,24 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
 
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return render_template('error.html')
+        except Exception as e:
+            db.session.rollback()
+            return render_template('index.html', error=str(e), tasks=Todo.query.all())
+    
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+        
 if __name__ == '__main__':
     app.run(debug=True)
